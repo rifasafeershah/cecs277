694910a4bf624c258e7d52edf60a51f1ce4b5b8b
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-19 00:14:46 -0800

    Create RectangleAdapter.java

---
+++ labs/Adapter/RectangleAdapter.java
@@ -0,0 +1,38 @@
+public class RectangleAdapter implements OldRectangle{ 
+	private NewRectangle rect;
+	public RectangleAdapter(Point first, Point second, Point third, Point fourth)
+	{
+		this.rect = new NewRectangle(first, first.getY() - third.getY(), second.getX() - first.getX());
+	}
+
+	@Override
+	public  double getArea()
+	{
+		return this.rect.getArea();
+	}
+
+	@Override
+	public double getperimeter() {
+		return this.rect.getPerimeter();
+	}
+
+	@Override
+	public double getDiagonal() {
+		return Math.sqrt(Math.pow(this.rect.getLength(), 2) + Math.pow(this.rect.getWidth(), 2));
+	}
+
+	@Override
+	public Point[] getCorners() {
+		Point upperLeft = this.rect.getUpperLeft();
+		Point upperRight = new Point(upperLeft.getX() + this.rect.getLength(), upperLeft.getY());
+		Point lowerLeft = new Point(upperLeft.getX(), upperLeft.getY() - this.rect.getWidth());
+		Point lowerRight = new Point(upperRight.getX(), upperRight.getY() - this.rect.getWidth());
+		Point[] points = new Point[4];
+		points[0] = upperLeft;
+		points[1] = upperRight;
+		points[2] = lowerLeft;
+		points[3] = lowerRight;
+		return points;
+	}
+	
+}

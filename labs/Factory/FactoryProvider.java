501c3c47971fe26065aff07c5befd6c278eb56a4
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-25 16:22:45 -0800

    Create FactoryProvider.java

---
+++ labs/Factory/FactoryProvider.java
@@ -0,0 +1,24 @@
+/**
+ * This creates an instance of the proper factory, based on the factory
+ * type passed in.  If an invalid factory type is passed in, we just 
+ * return a null, but that's highly unlikely since I'm using an enumeration
+ * to regulate the factory types that we'll accept.
+ * @author	Dave Brown
+ * @date	11/21/2019
+ */
+public class FactoryProvider {
+	public static AbstractFactory getFactory (FactoryType factoryType) {
+		AbstractFactory results;
+		switch (factoryType) {
+		case POINT_DIMENSIONS:
+			results =  new ConcreteFactoryPointDimensions();
+			break;
+		case POINTS:
+			results = new ConcreteFactoryPoints();
+			break;
+		default:
+			results = null;
+		}
+		return results;
+	} // End of the getFactory method
+} // End of the FactoryProvider class

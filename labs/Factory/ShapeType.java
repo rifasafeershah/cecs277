cf47e6db00a26bccd5333239934e1f0c75e695b0
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-25 16:29:28 -0800

    Create ShapeType.java

---
+++ labs/Factory/ShapeType.java
@@ -0,0 +1,9 @@
+/**
+ * The library of shape types that we support.  It is here
+ * that you will need to add any new shape type names.
+ * @author	David Brown
+ * @date	11/21/2019
+ */
+public enum ShapeType {
+	RECTANGLE, LINE_SEGMENT, CIRCLE
+}

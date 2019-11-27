8860c7c6eda149c2e26e8b000b6b13cf93225206
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-26 22:21:13 -0800

    Update CircleAbstractClass.java

--- labs/Factory/CircleAbstractClass.java
+++ labs/Factory/CircleAbstractClass.java
@@ -1,37 +1,62 @@
-public class CircleAbstractClass extends GeometricShape {
-	
-	@Override
-	public double getArea() {
-		// TODO Auto-generated method stub
-		return Math.PI * Math.pow(this.getRadius(),2);
-	}
+/**
+ * Circle abstract class that lays out the methods for measuring a circle.
+ * Lab Assignment: Factory
+ * @author Rifa Safeer Shah and Michael Zaragoza
+ * 11//25/19
+ */
 
-	@Override
-	public double getPerimeter() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+public abstract class CircleAbstractClass extends GeometricShape {
+    /**
+     * Gets the area of the circle.
+     * @return The area.
+     */
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(this.getRadius(), 2);
+    }
 
-	@Override
-	public Point getLocation() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Gets the perimeter of the circle.
+     * @return The perimeter.
+     */
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * this.getRadius();
+    }
 
-	@Override
-	public Point[] getPoints() {
-		// TODO Auto-generated method stub
-		return this.getPoints();
-	}
-	
-	public String toString () {
-		StringBuilder sb = new StringBuilder("Circle - Points: ");
-		Point [] corners = this.getRadius();
-		for (int i = 0; i < corners.length; i++) {
-			sb.append(corners[i].toString() + " ");
-		}
-		sb.append("\nArea: " + this.getArea() + " Perimeter: " + this.getPerimeter());
-		return sb.toString();
-	} // End of the toString method
+    /**
+     * Gets the center of the circle.
+     * @return The center point.
+     */
+    @Override
+    public Point getLocation() {
+        return this.getPoints()[0];
+    }
 
+    /**
+     * Gets the points of the circle.
+     * @return The points.
+     */
+    @Override
+    public abstract Point[] getPoints();
+
+    /**
+     * Gets the radius of the circle.
+     * @return The radius.
+     */
+    public abstract double getRadius();
+
+    /**
+     * Formats the measurements of a circle.
+     * @return String format of characteristics of a circle.
+     */
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Circle- Points: ");
+        Point [] points = this.getPoints();
+        for (int i = 0; i < points.length; i++) {
+            sb.append(points[i].toString() + " ");
+        } //prints coordinates of each point
+        sb.append("of radius: " + this.getRadius() + " Area: " + getArea() + " Perimeter: " + this.getPerimeter());
+        return sb.toString();
+    }
 }

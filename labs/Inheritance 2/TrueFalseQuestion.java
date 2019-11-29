a3390ff61706c8bdb21f93d02a6ee81a9562a659
Rifa Safeer Shah <rifashah28@gmail.com>
2019-11-28 16:07:19 -0800

    Create TrueFalseQuestion.java

---
+++ labs/Inheritance 2/TrueFalseQuestion.java
@@ -0,0 +1,13 @@
+
+public class TrueFalseQuestion extends Question
+{
+
+	public static void main(String[] args)
+	{
+		TrueFalseQuestion first = new TrueFalseQuestion();
+		first.setText("Today is Tuesday.");
+		String correctAnswer = "FALSE";
+
+	}
+
+}

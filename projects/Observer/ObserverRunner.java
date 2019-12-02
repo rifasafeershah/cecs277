6ad88ff089fc0b039434d27d9507a5fe1a7e87ed
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-01 17:25:14 -0800

    Update ObserverRunner.java

--- projects/Observer/ObserverRunner.java
+++ projects/Observer/ObserverRunner.java
@@ -1,1 +1,12 @@
+/**
+* Main program that runs the trade stock operation.
+* Homework Assignment: Observer
+* @author Rifa Safeer Shah and Michael Zaragoza
+* 12/02/19
+*/
+package cecs277observerHW;
 
+public class ObserverRunner {
+  public static void main(String[] args) {
+  }
+}

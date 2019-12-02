b6c80201ca33e8b56d09bd49b092d96facd36bd1
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-02 14:33:30 -0800

    Update Agent.java

--- projects/Observer/Agent.java
+++ projects/Observer/Agent.java
@@ -1,20 +1,36 @@
 /**
- * Agent class that allows stocks to be traded between one another..
+ * Agent class that allows stocks to be traded between one another
  * Homework Assignment: Observer
  * @author Rifa Safeer Shah and Michael Zaragoza
  * 12/02/19
  */
 package cecs277observerHW;
 
-import java.util.Observable;
-import java.util.Observer;
 
 public class Agent implements Observer {
     /** The name of the agent. */
     private String mAgentName;
+    /** the WeatherData instance that got us started as a subscriber */
+    private Subject mSubject;
 
+    /**
+     * Creates an agent.
+     * @param name The agent's name.
+     * @param stock The stock they will subscribe to.
+     */
+    public Agent(String name, Subject stock) {
+        this.mAgentName = name;
+        this.mSubject = stock;
+        stock.registerObserver(this);
+    }
+
+    /**
+     * Updates the agent about a stock trade.
+     * @param	stock The stock.
+     */
     @Override
-    public void update(Observable o, Object arg) {
-
+    public void update(Subject stock) {
+        Stock updatedStock = (Stock) stock;
+        System.out.println("Agent - name: " + mAgentName + " alerted to Bid: " + ((Stock) stock).getBid());
     }
 }

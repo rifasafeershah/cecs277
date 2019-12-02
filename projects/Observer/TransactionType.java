7dc24a0f3ae99f35bae6d031b920f92322f2ae66
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-01 17:20:42 -0800

    Update TransactionType.java

--- projects/Observer/TransactionType.java
+++ projects/Observer/TransactionType.java
@@ -1,1 +1,11 @@
+/**
+ * Enumeration class that describes the transaction type for the stock.
+ * Homework Assignment: Observer
+ * @author Rifa Safeer Shah and Michael Zaragoza
+ * 12/02/19
+ */
+package cecs277observerHW;
 
+public enum TransactionType {
+    BUY, SELL;
+}

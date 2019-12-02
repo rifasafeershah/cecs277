a384f25047c7c07bda27241a2a9dc60b78187cd1
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-02 14:34:26 -0800

    Update ObserverRunner.java

--- projects/Observer/ObserverRunner.java
+++ projects/Observer/ObserverRunner.java
@@ -1,12 +1,31 @@
 /**
-* Main program that runs the trade stock operation.
-* Homework Assignment: Observer
-* @author Rifa Safeer Shah and Michael Zaragoza
-* 12/02/19
-*/
+ * Main program that runs the trade stock operation.
+ * Homework Assignment: Observer
+ * @author Rifa Safeer Shah and Michael Zaragoza
+ * 12/02/19
+ */
 package cecs277observerHW;
 
 public class ObserverRunner {
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        //Creating the stocks
+        Stock orcl = new Stock("ORCL");
+        Stock ba = new Stock("BA");
+        Stock trxx = new Stock("TRXX");
+        //Creating the bids
+        Bid twenty = new Bid(orcl, 20, 4500, TransactionType.BUY);
+        Bid ten = new Bid(ba, 10, 3500, TransactionType.SELL);
+        Bid fifty = new Bid(trxx, 50, 5000, TransactionType.BUY);
+        //Creating the agents
+        Agent date = new Agent("Date", orcl);
+        Agent rifa = new Agent("Rifa", ba);
+        Agent richie = new Agent("Richie", trxx);
+        //Trading stocks
+        orcl.trade(twenty); //oracle.addobserve(rifa);
+        ba.trade(fifty);
+        trxx.trade(ten);
+        orcl.trade(ten);
+        ba.trade(twenty);
+        trxx.trade(fifty);
+    }
 }

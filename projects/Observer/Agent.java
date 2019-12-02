08cfe9560755dfdc698add70a8e022c858209b3f
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-01 17:19:11 -0800

    Update Agent.java

--- projects/Observer/Agent.java
+++ projects/Observer/Agent.java
@@ -1,1 +1,20 @@
+/**
+ * Agent class that allows stocks to be traded between one another..
+ * Homework Assignment: Observer
+ * @author Rifa Safeer Shah and Michael Zaragoza
+ * 12/02/19
+ */
+package cecs277observerHW;
 
+import java.util.Observable;
+import java.util.Observer;
+
+public class Agent implements Observer {
+    /** The name of the agent. */
+    private String mAgentName;
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}

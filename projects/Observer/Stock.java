31e544df8c30a71ae349998bdd6d5cf5e085963c
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-02 14:34:59 -0800

    Update Stock.java

--- projects/Observer/Stock.java
+++ projects/Observer/Stock.java
@@ -6,11 +6,33 @@
  */
 package cecs277observerHW;
 
-import java.util.Observable;
 
-public class Stock extends Observable {
+import java.util.ArrayList;
+
+public class Stock implements Subject {
     /** The bid for the stock */
     private Bid bid;
+    /** Our list of current observers. */
+    private ArrayList<Observer> observers;
+    /** The stock type */
+    private String mStockSymbol;
+
+    /**
+     * Creates a stock.
+     * @param symbol The stock symbol.
+     */
+    public Stock(String symbol) {
+        this.observers = new ArrayList<>();
+        this.mStockSymbol = symbol;
+    }
+
+    /**
+     * Gets the stock's symbol.
+     * @return The symbol.
+     */
+    public String getSymbol() {
+        return mStockSymbol;
+    }
 
     /**
      * Creates the stock.
@@ -18,6 +40,7 @@ public class Stock extends Observable {
      */
     public void trade(Bid bidObject) {
         this.bid = bidObject;
+        notifyObservers();
     }
 
     /**
@@ -27,5 +50,47 @@ public class Stock extends Observable {
     public Bid getBid() {
         Bid copyBid = this.bid;
         return copyBid;
+    }
+
+    /**
+     * Registers an observer to the list of observers.
+     * @param observer	The new observer.
+     * @return
+     */
+    @Override
+    public boolean registerObserver(Observer observer) {
+        if (observers.contains(observer)) {
+            return false;
+        }
+        else {
+            observers.add(observer);
+            return true;
+        }
+    }
+
+    /**
+     * Removes an observer to the list.
+     * @param	observer	The existing observer to remove.
+     * @return Boolean value if the observer was removed from the list.
+     */
+    @Override
+    public boolean removeObserver (Observer observer) {
+        if (observers.contains(observer)) {
+            observers.remove(observer);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * Notifies observers that changes have been made to the stock trades.
+     */
+    @Override
+    public void notifyObservers() {
+        for (Observer next: this.observers) {
+            next.update(this);
+        }
     }
 }

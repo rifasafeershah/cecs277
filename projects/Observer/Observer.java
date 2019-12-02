4e691a45c39f739f365d9513738a802a0afd7468
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-02 14:30:58 -0800

    Update Observer.java

--- projects/Observer/Observer.java
+++ projects/Observer/Observer.java
@@ -1,1 +1,15 @@
+/**
+ * Observer interface that allows observers to update all stock trades from their subjects.
+ * Homework Assignment: Observer
+ * @author Rifa Safeer Shah and Michael Zaragoza
+ * 12//02/19
+ */
+package cecs277observerHW;
 
+public interface Observer {
+    /**
+     * The method that the subject calls on each observer/subscriber to
+     * @param	stock The stock.
+     */
+    public void update (Subject stock);
+}

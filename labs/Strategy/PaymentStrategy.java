cb36ba51e5ce1892629d7d05d140d5dad8a0f038
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-04 23:07:33 -0800

    Update PaymentStrategy.java

--- labs/Strategy/PaymentStrategy.java
+++ labs/Strategy/PaymentStrategy.java
@@ -1,1 +1,12 @@
+/**
+ * Strategy interface that displays transactions with any payment strategy.
+ * Lab Assignment: Strategy
+ * @author Rifa Safeer Shah and Michael Zaragoza
+ * 12/04/19
+ */
+package StrategyPattern;
 
+public interface PaymentStrategy {
+    /** Returns the amount paid*/
+    void pay(double amount);
+}

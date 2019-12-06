1036aad5b5d4ccba67f9609622b1b6bb0963169d
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-06 13:31:12 -0800

    Update DeluxeRoom.java

--- projects/Term Project/DeluxeRoom.java
+++ projects/Term Project/DeluxeRoom.java
@@ -1,1 +1,12 @@
+package TermProject;
 
+public class DeluxeRoom extends Room {
+    private boolean doNotDistributed;
+
+    public DeluxeRoom() {
+        super();
+    }
+    public void setDoNotDistributed(boolean doNotDistributed) {
+        super.setDoNotDistributed(doNotDistributed);
+    }
+}

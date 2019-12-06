8ff820561793b6d7afbb7b59eb4eb02ab2ca5f5c
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-06 13:32:47 -0800

    Update RoachMotel.java

--- projects/Term Project/RoachMotel.java
+++ projects/Term Project/RoachMotel.java
@@ -1,1 +1,43 @@
+package TermProject;
 
+import java.util.ArrayList;
+
+public class RoachMotel implements Subject {
+    private ArrayList<Observer> observers;
+    private String motelName;
+    private int capacity;
+
+    public RoachMotel(String name, int capacity) {
+        this.motelName = name;
+        this.capacity = capacity;
+        //FIXME make an arraylist for rooms?
+    }
+    @Override
+    public boolean registerObserver(Observer observer) {
+        if (observers.contains(observer)) {
+            return false;
+        }
+        else {
+            observers.add(observer);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean removeObserver(Observer observer) {
+        if (observers.contains(observer)) {
+            observers.remove(observer);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer next: this.observers) {
+            next.update();
+        }
+    }
+}

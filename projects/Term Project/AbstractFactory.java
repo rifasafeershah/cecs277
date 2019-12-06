bea04726179397d305f46bc3d3556940b2cca22e
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-06 13:29:34 -0800

    Update AbstractFactory.java

--- projects/Term Project/AbstractFactory.java
+++ projects/Term Project/AbstractFactory.java
@@ -1,1 +1,9 @@
+package TermProject;
 
+public abstract class AbstractFactory {
+    /**
+     *
+     */
+    //FIXME fix this
+    public abstract Room getRoom (RoomType roomType);
+} // End of the AbstractFactory class

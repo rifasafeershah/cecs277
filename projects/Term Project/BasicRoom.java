f5c423389dcd8864613a9ee09baa5b6e4ac72489
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-06 13:33:40 -0800

    Update BasicRoom.java

--- projects/Term Project/BasicRoom.java
+++ projects/Term Project/BasicRoom.java
@@ -1,1 +1,12 @@
+package TermProject;
 
+public class BasicRoom extends Room {
+    private boolean doNotDistributed;
+
+    public BasicRoom() {
+        super();
+    }
+    public void setDoNotDistributed(boolean doNotDistributed) {
+        super.setDoNotDistributed(doNotDistributed);
+    }
+}

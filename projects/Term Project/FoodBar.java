16592a847541b045400ddf957609db565a425655
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:16:31 -0800

    Update FoodBar.java

--- projects/Term Project/FoodBar.java
+++ projects/Term Project/FoodBar.java
@@ -1,32 +1,57 @@
 /**
- * Observer class represents an observer watching over a subject.
+ * Food bar that decorates a room as an amenity.
  * Homework Assignment: Term Project
  * @author Michael Zaragoza and Rifa Safeer Shah
  * 12/09/2019
  */
-package TermProject;
+package cecs277termproject;
 
 public class FoodBar extends RoomDecorator {
-
+    /**
+     * Creates a Food Bar.
+     * @param room The room.
+     */
     public FoodBar(Room room) {
         super(room);
     }
-
+    /**
+     * Gets the room number.
+     * @return The room number.
+     */
     public String getRoomNumber() {
         return super.getDecoratedRoom().getRoomNumber();
     }
-
+    /**
+     * Gets the room type.
+     * @return The room type.
+     */
     public RoomType getRoomType() {
         return super.getDecoratedRoom().getRoomType();
     }
-
+    /**
+     * Prints the room description.
+     * @return The room description.
+     */
     @Override
     public String toString() {
         return super.getDecoratedRoom() + ", Food bar";
     }
-
+    /**
+     * Calculates the cost of the room with the spa amenity included.
+     * @return the total cost.
+     */
     @Override
     public double cost() {
         return 5.00 + super.getDecoratedRoom().cost();
+    }
+
+    /**
+     * Accepts the maid(visitor) and cleans the room.
+     * @param visitor The maid visiting the room.
+     * @return Status of the room.
+     */
+    @Override
+    public String accept(CleaningRoomVisitor visitor) {
+        return super.getDecoratedRoom().accept(visitor);
     }
 }

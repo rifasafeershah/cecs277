79046faaa8b3069a82469d37dcbf08f2cef18e4b
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:18:06 -0800

    Update PaymentStrategy.java

--- projects/Term Project/PaymentStrategy.java
+++ projects/Term Project/PaymentStrategy.java
@@ -1,6 +1,12 @@
-package TermProject;
+/**
+ * Payment strategy interface that allows customers to pay in different ways.
+ * Homework Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package cecs277termproject;
 
 public interface PaymentStrategy {
     /** Returns the amount paid*/
-    void pay(double amount);
+    void pay(double amount, int nights);
 }

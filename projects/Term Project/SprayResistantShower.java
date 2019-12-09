a1739db6e1dbfd28e9feacbaad0e24e5d7beae7a
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 23:50:22 -0800

    Update SprayResistantShower.java

--- projects/Term Project/SprayResistantShower.java
+++ projects/Term Project/SprayResistantShower.java
@@ -1,1 +1,18 @@
+package cecs277termproject;
 
+public class SprayResistantShower extends RoomDecorator {
+
+    public SprayResistantShower(Room room) {
+        super(room);
+    }
+
+    @Override
+    public String toString() {
+        return super.getDecoratedRoom() + ", Shower";
+    }
+
+    @Override
+    public double cost() {
+        return 25.00 + super.getDecoratedRoom().cost();
+    }
+}

e4e0d94f6b8c06bcc2f938d5998963073e92596c
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:16:01 -0800

    Update FactoryProvider.java

--- projects/Term Project/FactoryProvider.java
+++ projects/Term Project/FactoryProvider.java
@@ -1,37 +1,33 @@
 /**
- * 
+ * FactoryProvider class adds new rooms of different types and sets it to a room number.
  * Homework Assignment: Term Project
  * @author Michael Zaragoza and Rifa Safeer Shah
  * 12/09/2019
  */
-
 package cecs277termproject;
 
-//---------------------------------------------------------------------------------------------------------------------------
-//FactoryProvider.java
-/**
-* Observer class represents an observer watching over a subject.
-* Homework Assignment: Term Project
-* @author Michael Zaragoza and Rifa Safeer Shah
-* 12/09/2019
-*/
-
 public class FactoryProvider {
-public static Room getRoom (RoomType roomType, String roomNumber) {
-    Room results;
-    switch (roomType) {
-        case BASIC:
-            results = new BasicRoom(roomNumber);
-            break;
-        case DELUXE:
-            results = new DeluxeRoom(roomNumber);
-            break;
-        case SUITE:
-            results = new Suite(roomNumber);
-            break;
-        default:
-            results = null;
-    }
-    return results;
-} // End of the getRoom method
+    /**
+     * Produces a type of room.
+     * @param roomType The typr of room requested.
+     * @param roomNumber The room number.
+     * @return The desired room.
+     */
+    public static Room getRoom (RoomType roomType, String roomNumber) {
+        Room results;
+        switch (roomType) {
+            case BASIC:
+                results = new BasicRoom(roomNumber);
+                break;
+            case DELUXE:
+                results = new DeluxeRoom(roomNumber);
+                break;
+            case SUITE:
+                results = new Suite(roomNumber);
+                break;
+            default:
+                results = null;
+        }
+        return results;
+    } // End of the getRoom method
 } // End of the FactoryProvider class

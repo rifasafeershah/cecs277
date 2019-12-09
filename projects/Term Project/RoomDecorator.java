567fed8047051d957564a0756dd525687286bbf7
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:21:16 -0800

    Update RoomDecorator.java

--- projects/Term Project/RoomDecorator.java
+++ projects/Term Project/RoomDecorator.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Room decorator class that decorates a motel room with amenities.
  * Homework Assignment: Term Project
  * @author Michael Zaragoza and Rifa Safeer Shah
  * 12/09/2019
@@ -7,12 +7,21 @@
 package cecs277termproject;
 
 public abstract class RoomDecorator extends Room {
+    /** The motel room*/
     private Room decoratedRoom;
 
+    /**
+     * Creates a decorated room.
+     * @param decoratedRoom The decorated motel room.
+     */
     public RoomDecorator (Room decoratedRoom) {
         this.decoratedRoom = decoratedRoom;
     }
-    
+
+    /**
+     * Gets the decorated room.
+     * @return The decorated motel room.
+     */
     public Room getDecoratedRoom() {
         return this.decoratedRoom;
     }

d84b15e5e0f0d4464244151dab7f4d0d5f3dff89
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 23:43:40 -0800

    Update RefillBar.java

--- projects/Term Project/RefillBar.java
+++ projects/Term Project/RefillBar.java
@@ -1,1 +1,32 @@
+/**
+ * RefillBar class 
+ * Homewrok Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package cecs277termproject;
 
+public class RefillBar extends RoomDecorator {
+
+    public RefillBar(Room room) {
+        super(room);
+    }
+    
+    public String getRoomNumber() {
+    	return super.getDecoratedRoom().getRoomNumber();
+    }
+    
+    public RoomType getRoomType() {
+    	return super.getDecoratedRoom().getRoomType();
+    }
+    
+    @Override
+    public String toString() {
+        return super.getDecoratedRoom() + ", Refill Bar";
+    }
+
+    @Override
+    public double cost() {
+        return 5.00 + super.getDecoratedRoom().cost();
+    }
+} //End of the RefillBar class

09bfbe6a94dac97a14ae23b8670eb2e3b327b197
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 23:39:33 -0800

    Update RoomDecorator.java

--- projects/Term Project/RoomDecorator.java
+++ projects/Term Project/RoomDecorator.java
@@ -1,4 +1,26 @@
-package TermProject;
+/**
+ * 
+ * Homework Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package cecs277termproject;
 
 public abstract class RoomDecorator extends Room {
+    private Room decoratedRoom;
+
+    public RoomDecorator (Room decoratedRoom) {
+        this.decoratedRoom = decoratedRoom;
+    }
+    
+    public Room getDecoratedRoom() {
+        return this.decoratedRoom;
+    }
+
+    /**
+     * Throws an error if nothing is in the decorator.
+     */
+    public RoomDecorator () {
+        throw new IllegalArgumentException ("Must supply something to decorate!");
+    }
 }

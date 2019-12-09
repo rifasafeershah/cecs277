e1c7dedb627cb89c3940b71a381246b8ead02d06
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:14:37 -0800

    Update CleaningRoomVisitor.java

--- projects/Term Project/CleaningRoomVisitor.java
+++ projects/Term Project/CleaningRoomVisitor.java
@@ -1,11 +1,16 @@
 /**
- * TransactionLogger class records all the transactions for the motel.
+ * CleaningRoomVisitor class visits all the rooms in the motel.
  * Homework Assignment: Term Project
  * @author Michael Zaragoza and Rifa Safeer Shah
  * 12/09/2019
  */
+package cecs277termproject;
+
 public interface CleaningRoomVisitor {
-  String visit(BasicRoom room);
-  String visit(DeluxeRoom room);
-  String visit(Suite room);
-}
+    //Visiting the Basic room
+    String visit(BasicRoom room);
+    //Visiting the Deluxe room
+    String visit(DeluxeRoom room);
+    //visiting the Suite room
+    String visit(Suite room);
+} // End of the CleaningRoomVisitor class

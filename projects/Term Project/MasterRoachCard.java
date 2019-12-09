1a73782d815fc77ead1acabbde50dfcd267e7c91
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:17:14 -0800

    Update MasterRoachCard.java

--- projects/Term Project/MasterRoachCard.java
+++ projects/Term Project/MasterRoachCard.java
@@ -1,34 +1,30 @@
-package TermProject;
+/**
+ * MasterRoachCard class that implements PaymentStrategy for the Strategy pattern.
+ * Homework Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package cecs277termproject;
 
 public class MasterRoachCard implements PaymentStrategy {
     /** The name of the card user */
     private String name;
-    /** The card number of the card */
-    private String cardNumber;
-    /** The security code of the card */
-    private String cvv;
-    /** The date of expiration of the card */
-    private String dateOfExpiry;
 
     /**
      * Creates a new credit card.
      * @param name The name of the colony.
-     * @param cardNumber The card number.
-     * @param cvv The security code for the card.
-     * @param dateOfExpiry The date of expiration of the card.
      */
-    public MasterRoachCard(String name, String cardNumber, String cvv, String dateOfExpiry) {
+    public MasterRoachCard(String name) {
         this.name = name;
-        this.cardNumber = cardNumber;
-        this.cvv = cvv;
-        this.dateOfExpiry = dateOfExpiry;
     }
     /**
      * Returns amount paid with buyer's info.
      * @param amount The amount paid.
      */
     @Override
-    public void pay(double amount) {
-        System.out.println("You have paid $" + amount + " with your credit card with Name: " + this.name + " and with Card Number: " + this.cardNumber);
+    public void pay(double amount, int nights) {
+        String transaction = this.name + " has paid $" + amount + " for " + nights + " night(s) with a debit/credit card";
+        TransactionLogger.getLogger().log(transaction);
+        System.out.println(transaction);
     }
 }

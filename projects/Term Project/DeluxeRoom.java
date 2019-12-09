355c1915b7baa61c8f0b4ccdada01bf9d87e6ffc
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 16:28:39 -0800

    Update DeluxeRoom.java

--- projects/Term Project/DeluxeRoom.java
+++ projects/Term Project/DeluxeRoom.java
@@ -6,7 +6,18 @@ public class DeluxeRoom extends Room {
     public DeluxeRoom() {
         super();
     }
+
     public void setDoNotDistributed(boolean doNotDistributed) {
         super.setDoNotDistributed(doNotDistributed);
     }
-}
+
+    @Override
+    public String toString() {
+        return "Deluxe Room";
+    }
+
+    @Override
+    public double cost() {
+        return 90;
+    }
+} //End of the DeluxeRoom class

e222aca1e664398c3615ef70d2f99b887dd6e64d
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 23:45:11 -0800

    Update RoachPal.java

--- projects/Term Project/RoachPal.java
+++ projects/Term Project/RoachPal.java
@@ -1,1 +1,26 @@
+package cecs277termproject;
 
+public class RoachPal implements PaymentStrategy {
+    /** The name */
+    private String name;
+    /** The email of the buyer */
+    private String emailID;
+
+    /**
+     * Creates a PayPal account.
+     * @param name The buyer's colony name
+     * @param emailID The buyer's email.
+     */
+    public RoachPal(String name, String emailID) {
+        this.name = name;
+        this.emailID = emailID;
+    }
+    /**
+     * Returns amount paid with buyer's info.
+     * @param amount The amount paid.
+     */
+    @Override
+    public void pay(double amount) {
+        System.out.println(this.name + " has paid $" + amount + " with RoachPal" );
+    }
+}

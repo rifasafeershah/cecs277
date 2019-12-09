c4db2ee4421826f03a69329fdf0ffd2891e52bda
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 23:51:45 -0800

    Update Suite.java

--- projects/Term Project/Suite.java
+++ projects/Term Project/Suite.java
@@ -1,1 +1,23 @@
+package cecs277termproject;
 
+public class Suite extends Room {
+    private boolean doNotDisturb;
+
+    public Suite() {
+        super(roomNumber);
+    }
+
+    public void setDoNotDistributed(boolean doNotDisturb) {
+        super.setDoNotDisturb(doNotDisturb);
+    }
+
+    @Override
+    public String toString() {
+        return "Suite";
+    }
+
+    @Override
+    public double cost() {
+        return 100;
+    }
+}

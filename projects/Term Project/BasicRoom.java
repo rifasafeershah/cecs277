f1ab8ff99d667b5f2909dded016d2951f1a4e1de
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:14:13 -0800

    Update BasicRoom.java

--- projects/Term Project/BasicRoom.java
+++ projects/Term Project/BasicRoom.java
@@ -1,23 +1,78 @@
-package TermProject;
+/**
+ * BasicRoom class extends Room class and represents a basic room in the motel.
+ * Homework Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package cecs277termproject;
 
 public class BasicRoom extends Room {
-    private boolean doNotDistributed;
-
-    public BasicRoom() {
-        super();
+    /**
+     * Creates a basic room.
+     * @param roomNumber The room number.
+     */
+    public BasicRoom(String roomNumber) {
+        super(roomNumber);
+    }
+    /**
+     * Returns a room number for the motel room.
+     * @return The room number.
+     */
+    public String getRoomNumber() {
+        return super.getRoomNumber();
     }
 
-    public void setDoNotDistributed(boolean doNotDistributed) {
-        super.setDoNotDistributed(doNotDistributed);
+    /**
+     * Gets the room type.
+     * @return The room type.
+     */
+    @Override
+    public RoomType getRoomType() {
+        return RoomType.BASIC;
     }
 
+    /**
+     * Sets the "Do Not Disturb" sign.
+     * @param doNotDisturb Boolean value.
+     */
+    public void setDoNotDisturb(boolean doNotDisturb) {
+        super.setDoNotDisturb(doNotDisturb);
+    }
+    /**
+     * Prints information about the room.
+     * @return String value of the room type.
+     */
     @Override
     public String toString() {
-        return "Basic Room";
+        return super.getRoomNumber() + ": Basic Room";
     }
-
+    /**
+     * Calculates the total cost of the motel room.
+     * @return The total cost.
+     */
     @Override
     public double cost() {
         return 80;
     }
-} //End of the BasicRoom class
+    /**
+     * Sets the status of the room having a spray resistant shower.
+     * @param value Boolean value.
+     */
+    public void setHasShower(boolean value) {
+        super.setHasShower(value);
+    }
+    /**
+     * Accepts the maid (visitor) and cleans the room.
+     * @param visitor The maid visiting the rood.
+     * @return Status of the room.
+     */
+    public String accept(CleaningRoomVisitor visitor) {
+        if (!super.getDoNotDisturb()) {
+            System.out.println("Visiting room " + getRoomNumber());
+            return visitor.visit(this);
+        }
+        else {
+            return "Do not Clean";
+        }
+    }
+}

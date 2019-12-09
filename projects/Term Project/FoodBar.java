7d414cd8b6970cbc973574b81677668ea4da407d
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 23:34:11 -0800

    Update FoodBar.java

--- projects/Term Project/FoodBar.java
+++ projects/Term Project/FoodBar.java
@@ -1,1 +1,32 @@
+/**
+ * Observer class represents an observer watching over a subject.
+ * Homework Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package TermProject;
 
+public class FoodBar extends RoomDecorator {
+
+    public FoodBar(Room room) {
+        super(room);
+    }
+
+    public String getRoomNumber() {
+        return super.getDecoratedRoom().getRoomNumber();
+    }
+
+    public RoomType getRoomType() {
+        return super.getDecoratedRoom().getRoomType();
+    }
+
+    @Override
+    public String toString() {
+        return super.getDecoratedRoom() + ", Food bar";
+    }
+
+    @Override
+    public double cost() {
+        return 5.00 + super.getDecoratedRoom().cost();
+    }
+}

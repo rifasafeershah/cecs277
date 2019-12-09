2cc6d293d632264a301cd8756ca6ef7418526798
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:21:44 -0800

    Update RoomType.java

--- projects/Term Project/RoomType.java
+++ projects/Term Project/RoomType.java
@@ -1,1 +1,11 @@
+/**
+ * Enumeration of different room types.
+ * Homework Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package cecs277termproject;
 
+public enum RoomType {
+    BASIC, DELUXE, SUITE;
+}

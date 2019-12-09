56769d9f90b2c5c8c3e6ab3a828cfa41b317e857
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:17:42 -0800

    Update Observer.java

--- projects/Term Project/Observer.java
+++ projects/Term Project/Observer.java
@@ -1,6 +1,14 @@
-package TermProject;
+/**
+ * Observer interface that allows customers to get notifications on any available motel rooms.
+ * Homework Assignment: Term Project
+ * @author Michael Zaragoza and Rifa Safeer Shah
+ * 12/09/2019
+ */
+package cecs277termproject;
 
 public interface Observer {
-
+    /**
+     * Updates the customers on any available motel rooms.
+     */
     public void update ();
 }

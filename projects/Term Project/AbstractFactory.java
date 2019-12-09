65520beaae51c3d8ebd395eef8ebeb59fdd37baf
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-09 14:13:52 -0800

    Update AbstractFactory.java

--- projects/Term Project/AbstractFactory.java
+++ projects/Term Project/AbstractFactory.java
@@ -1,11 +1,17 @@
 /**
- * Observer class represents an observer watching over a subject.
+ * AbstractFactory class gets a specific room in the motel.
  * Homework Assignment: Term Project
  * @author Michael Zaragoza and Rifa Safeer Shah
  * 12/09/2019
  */
-package TermProject;
-public abstract class AbstractFactory {
-    public abstract Room getRoom (RoomType roomType);
+package cecs277termproject;
 
-} //End of the AbstractFactory class
+public abstract class AbstractFactory {
+    /**
+     * Produces a room specified in the parameters.
+     * @param roomType The type of room.
+     * @param roomNumber The motel room number.
+     * @return The class with the room type specified.
+     */
+    public abstract Room getRoom (RoomType roomType, String roomNumber);
+} // End of the AbstractFactory class

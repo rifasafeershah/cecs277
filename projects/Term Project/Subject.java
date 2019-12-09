1705203746aa251ce4c4df236ffa47796ae99680
Rifa Safeer Shah <rifashah28@gmail.com>
2019-12-08 23:44:37 -0800

    Update Subject.java

--- projects/Term Project/Subject.java
+++ projects/Term Project/Subject.java
@@ -1,1 +1,22 @@
+package cecs277termproject;
 
+public interface Subject {
+    /**
+     * Add a new observer to our list of subscribers.
+     * @param	observer	The new observer.
+     * @return				True if the observer could be added, false otherwise.
+     */
+    public boolean registerObserver (Observer observer);
+
+    /**
+     * Remove an existing observer (unsubscribe them).
+     * @param	observer	The existing observer to remove.
+     * @return				True if we found them, false otherwise.
+     */
+    public boolean removeObserver (Observer observer);
+
+    /**
+     * Pass along the new values to all of the registered observers.
+     */
+    public void notifyObservers();
+}
